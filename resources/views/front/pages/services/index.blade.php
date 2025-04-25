4aa129f6f8cce980935c11571f46c0dbddbbf09f
nusratnova5 <nusrat.nova16@gmail.com>
2025-04-25 19:55:29 +0600

    services page's content changed

--- resources/views/front/pages/services/index.blade.php
+++ resources/views/front/pages/services/index.blade.php
@@ -4,8 +4,8 @@
     <div class="hero-main-rp container">
         <div class="row">
             <div class="col-md-8">
-                <h1>Digital Agency Services</h1>
-                <p>Experience the Florida-sized value of Design Layer’s industry renowned custom software and internet marketing experts. Our digital agency has worked on over 1500 successful digital projects for more than 500 businesses.</p>
+                <h1>Collaborative Business Solutions</h1>
+                <p>At FIVEPCMIRACLE LIMITED, we believe that "Collaboration is the Key" to driving innovation, growth, and profitability. We specialize in connecting businesses and customers, fostering partnerships, and integrating operations to unlock new opportunities for over 500 businesses across multiple industries.</p>
             </div>
         </div>
     </div>
@@ -14,7 +14,7 @@
 @include('front.shared.about-section-three')
 <section class="portfolio-inner-section">
 	<div class="hero-main-rp container">
-		<h2><img src="assets/images/Group 198.png" class="lazy"><br>Our Creative Display On Exhibit</h2>
+		<h2><img src="assets/images/Group 198.png" class="lazy"><br>Our Partnership Programs</h2>
 		<div class="owl-carousel owl-theme portfolio-slider owl-loaded owl-drag">
 			
 			
@@ -26,133 +26,133 @@
 		<div class="owl-stage-outer"><div class="owl-stage" style="transform: translate3d(-2100px, 0px, 0px); transition: all; width: 8400px;"><div class="owl-item cloned" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home lazy" style="background: url('assets/images/workfour.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://one-delivery.co.uk/" class="res"><img src="assets/images/worklogofour.svg" alt="ui and ux designing" class="lazy"></a>
-						<h3>All your favourite cuisines in one place</h3>
-						<a href="https://one-delivery.co.uk/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://one-delivery.co.uk/" class="res"><img src="assets/images/worklogofour.svg" alt="Channel Partner Program" class="lazy"></a>
+						<h3>Channel Partner Program</h3>
+						<a href="https://one-delivery.co.uk/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Channel Partner Program" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskfive.png" data-src="assets/images/maskfive.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskfive.png" data-src="assets/images/maskfive.png" alt="Channel Partner Program" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item cloned" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home" style="background: url('assets/images/workfive.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.madeofsundays.com/" class="res"><img src="assets/images/worklogofive.svg" alt="web app development" class="lazy"></a>
-						<h3>DECORATE WITH PAPER WALL DECALS</h3>
-						<a href="https://www.madeofsundays.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.madeofsundays.com/" class="res"><img src="assets/images/worklogofive.svg" alt="Distribution Projects" class="lazy"></a>
+						<h3>Distribution Projects</h3>
+						<a href="https://www.madeofsundays.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Distribution Projects" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskfour.png" data-src="assets/images/maskfour.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskfour.png" data-src="assets/images/maskfour.png" alt="Distribution Projects" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item cloned" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home" style="background: url('assets/images/worksix1.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://airbit.com/" class="res"><img src="assets/images/worklogosix1.png" alt="web designing" class="lazy"></a>
-						<h3>Find the perfect beat for your project</h3>
-						<a href="https://airbit.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://airbit.com/" class="res"><img src="assets/images/worklogosix1.png" alt="Reseller Program" class="lazy"></a>
+						<h3>Reseller Program</h3>
+						<a href="https://airbit.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Reseller Program" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskthree1.png" data-src="assets/images/maskthree1.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskthree1.png" data-src="assets/images/maskthree1.png" alt="Reseller Program" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item active" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home" style="background: url('assets/images/workone1.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.anoc.ae/" class="res"><img src="assets/images/worklogoone1.png" alt="web designing" class="lazy"></a>
-						<h3>Management Consultants</h3>
-						<a href="https://www.anoc.ae/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.anoc.ae/" class="res"><img src="assets/images/worklogoone1.png" alt="Wholesaler Program" class="lazy"></a>
+						<h3>Wholesaler Program</h3>
+						<a href="https://www.anoc.ae/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Wholesaler Program" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskone1.png" data-src="assets/images/maskone1.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskone1.png" data-src="assets/images/maskone1.png" alt="Wholesaler Program" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item active" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home lazy" style="background: url('assets/images/worktwo.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.kiewit.com/" class="res"><img src="assets/images/worklogotwo.svg" alt="web development" class="lazy"></a>
-						<h3>unmatched performance in transportation</h3>
-						<a href="https://www.kiewit.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.kiewit.com/" class="res"><img src="assets/images/worklogotwo.svg" alt="White Label Branding Services" class="lazy"></a>
+						<h3>White Label Branding Services</h3>
+						<a href="https://www.kiewit.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="White Label Branding Services" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/masktwo.png" data-src="assets/images/masktwo.png" alt="mockup" class="myLazy">
+						<img src="assets/images/masktwo.png" data-src="assets/images/masktwo.png" alt="White Label Branding Services" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home lazy" style="background: url('assets/images/workthree.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.crateandbarrel.com/" class="res"><img src="assets/images/worklogothree.svg" alt="web app" class="lazy"></a>
-						<h3>new &amp; designed with purpose</h3>
-						<a href="https://www.crateandbarrel.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.crateandbarrel.com/" class="res"><img src="assets/images/worklogothree.svg" alt="Digital Marketing Services" class="lazy"></a>
+						<h3>Digital Marketing Services</h3>
+						<a href="https://www.crateandbarrel.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Digital Marketing Services" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/cratemask.png" data-src="assets/images/cratemask.png" alt="mockup" class="myLazy">
+						<img src="assets/images/cratemask.png" data-src="assets/images/cratemask.png" alt="Digital Marketing Services" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home lazy" style="background: url('assets/images/workfour.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://one-delivery.co.uk/" class="res"><img src="assets/images/worklogofour.svg" alt="ui and ux designing" class="lazy"></a>
-						<h3>All your favourite cuisines in one place</h3>
-						<a href="https://one-delivery.co.uk/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://one-delivery.co.uk/" class="res"><img src="assets/images/worklogofour.svg" alt="Innovation Through Shared Expertise" class="lazy"></a>
+						<h3>Innovation Through Shared Expertise</h3>
+						<a href="https://one-delivery.co.uk/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Innovation Through Shared Expertise" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskfive.png" data-src="assets/images/maskfive.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskfive.png" data-src="assets/images/maskfive.png" alt="Innovation Through Shared Expertise" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home" style="background: url('assets/images/workfive.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.madeofsundays.com/" class="res"><img src="assets/images/worklogofive.svg" alt="web app development" class="lazy"></a>
-						<h3>DECORATE WITH PAPER WALL DECALS</h3>
-						<a href="https://www.madeofsundays.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.madeofsundays.com/" class="res"><img src="assets/images/worklogofive.svg" alt="Expanded Networks" class="lazy"></a>
+						<h3>Expanded Networks</h3>
+						<a href="https://www.madeofsundays.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Expanded Networks" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskfour.png" data-src="assets/images/maskfour.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskfour.png" data-src="assets/images/maskfour.png" alt="Expanded Networks" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home" style="background: url('assets/images/worksix1.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://airbit.com/" class="res"><img src="assets/images/worklogosix1.png" alt="web designing" class="lazy"></a>
-						<h3>Find the perfect beat for your project</h3>
-						<a href="https://airbit.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://airbit.com/" class="res"><img src="assets/images/worklogosix1.png" alt="Cost Efficiency" class="lazy"></a>
+						<h3>Cost Efficiency</h3>
+						<a href="https://airbit.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Cost Efficiency" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskthree1.png" data-src="assets/images/maskthree1.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskthree1.png" data-src="assets/images/maskthree1.png" alt="Cost Efficiency" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item cloned" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home" style="background: url('assets/images/workone1.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.anoc.ae/" class="res"><img src="assets/images/worklogoone1.png" alt="web designing" class="lazy"></a>
-						<h3>Management Consultants</h3>
-						<a href="https://www.anoc.ae/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.anoc.ae/" class="res"><img src="assets/images/worklogoone1.png" alt="Accelerated Growth" class="lazy"></a>
+						<h3>Accelerated Growth</h3>
+						<a href="https://www.anoc.ae/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Accelerated Growth" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/maskone1.png" data-src="assets/images/maskone1.png" alt="mockup" class="myLazy">
+						<img src="assets/images/maskone1.png" data-src="assets/images/maskone1.png" alt="Accelerated Growth" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item cloned" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home lazy" style="background: url('assets/images/worktwo.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.kiewit.com/" class="res"><img src="assets/images/worklogotwo.svg" alt="web development" class="lazy"></a>
-						<h3>unmatched performance in transportation</h3>
-						<a href="https://www.kiewit.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.kiewit.com/" class="res"><img src="assets/images/worklogotwo.svg" alt="Competitive Advantage" class="lazy"></a>
+						<h3>Competitive Advantage</h3>
+						<a href="https://www.kiewit.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Competitive Advantage" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/masktwo.png" data-src="assets/images/masktwo.png" alt="mockup" class="myLazy">
+						<img src="assets/images/masktwo.png" data-src="assets/images/masktwo.png" alt="Competitive Advantage" class="myLazy">
 					</div>
 				</div>
 			</div></div><div class="owl-item cloned" style="width: 670px; margin-right: 30px;"><div class="work-boxes-home lazy" style="background: url('assets/images/workthree.jpg');" loading="lazy">
 				<div class="inner-row">
 					<div class="col-md-7">
-						<a href="https://www.crateandbarrel.com/" class="res"><img src="assets/images/worklogothree.svg" alt="web app" class="lazy"></a>
-						<h3>new &amp; designed with purpose</h3>
-						<a href="https://www.crateandbarrel.com/" class="btn-website">view website <img src="assets/images/whitearrow.svg" alt="portfolio" class="lazy"></a>
+						<a href="https://www.crateandbarrel.com/" class="res"><img src="assets/images/worklogothree.svg" alt="Building Collaborative Ecosystems" class="lazy"></a>
+						<h3>Building Collaborative Ecosystems</h3>
+						<a href="https://www.crateandbarrel.com/" class="btn-website">Learn More <img src="assets/images/whitearrow.svg" alt="Building Collaborative Ecosystems" class="lazy"></a>
 					</div>
 					<div class="col-md-5">
-						<img src="assets/images/cratemask.png" data-src="assets/images/cratemask.png" alt="mockup" class="myLazy">
+						<img src="assets/images/cratemask.png" data-src="assets/images/cratemask.png" alt="Building Collaborative Ecosystems" class="myLazy">
 					</div>
 				</div>
 			</div></div></div></div><div class="owl-nav"><button type="button" role="presentation" class="owl-prev"><img src="/assets/images/left-arrow-abt.svg"></button><button type="button" role="presentation" class="owl-next"><img src="/assets/images/right-arrow-abt.svg"></button></div><div class="owl-dots disabled"></div></div>
